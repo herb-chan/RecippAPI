3c3f13251a01dff454e8577fd8f3b96b47a1483a
herb <wujowujo3@gmail.com>
2024-07-26 14:25:54 +0200

    nutritional informations update v0.2.0

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,18 +4,18 @@ const sequelize = require("../sequelize");
 /**
  * Represents a Recipe.
  * @typedef {Object} Recipe
- * @property {string} title - The title of the recipe.
- * @property {string} description - A brief description of the recipe.
- * @property {string} imagePath - The file path to an image representing the recipe.
- * @property {string} prepTime - The time required to prepare the recipe.
- * @property {string} servingSize - The number of servings the recipe yields.
- * @property {string} category - The category or type of the recipe (e.g., dessert, main course).
- * @property {Array<Object>} ingredients - An array of ingredient objects used in the recipe.
- * @property {Object} nutrition - An object containing nutritional information (e.g., calories, fat).
- * @property {Array<Object>} steps - An array of step-by-step instructions for preparing the recipe.
- * @property {Array<string>} [allergies] - An optional array of allergens that the recipe might contain.
- * @property {Array<string>} [diets] - An optional array of dietary categories the recipe fits (e.g., vegan, gluten-free).
- * @property {number} starCount - The number of stars given to the recipe, indicating its popularity or rating.
+ * @property {string} title The title of the recipe.
+ * @property {string} description A brief description of the recipe.
+ * @property {string} imagePath The file path to an image representing the recipe.
+ * @property {number} prepTime The time required to prepare the recipe in minutes.
+ * @property {number} servingSize The number of servings the recipe yields.
+ * @property {string} category The category or type of the recipe (e.g., dessert, main course).
+ * @property {Array<Object>} ingredients An array of ingredient objects used in the recipe.
+ * @property {Object} nutrition An object of nutritional information.
+ * @property {Array<string>} steps An array of step-by-step instructions for preparing the recipe.
+ * @property {Array<string>} [allergies] An optional array of allergens that the recipe might contain.
+ * @property {Array<string>} [diets] An optional array of dietary categories the recipe fits (e.g., vegan, gluten-free).
+ * @property {number} starCount The number of stars given to the recipe, indicating its popularity or rating.
  */
 
 /**
@@ -36,11 +36,11 @@ const Recipe = sequelize.define("Recipe", {
         allowNull: false,
     },
     prepTime: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
     },
     servingSize: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
     },
     category: {
@@ -50,22 +50,65 @@ const Recipe = sequelize.define("Recipe", {
     ingredients: {
         type: DataTypes.JSON,
         allowNull: false,
+        validate: {
+            isArray(value) {
+                if (!Array.isArray(value)) {
+                    throw new Error("Ingredients must be an array");
+                }
+            },
+        },
     },
     nutrition: {
         type: DataTypes.JSON,
         allowNull: false,
+        validate: {
+            isValidNutrition(value) {
+                if (typeof value !== "object") {
+                    throw new Error("Nutrition must be an object");
+                }
+                const requiredFields = ["calories", "fat", "carbs", "protein"];
+                requiredFields.forEach((field) => {
+                    if (!value[field] || typeof value[field] !== "object") {
+                        throw new Error(
+                            `Nutrition must include a valid ${field} object`
+                        );
+                    }
+                });
+            },
+        },
     },
     steps: {
         type: DataTypes.JSON,
         allowNull: false,
+        validate: {
+            isArray(value) {
+                if (!Array.isArray(value)) {
+                    throw new Error("Steps must be an array");
+                }
+            },
+        },
     },
     allergies: {
         type: DataTypes.JSON,
         allowNull: true,
+        validate: {
+            isArray(value) {
+                if (value !== null && !Array.isArray(value)) {
+                    throw new Error("Allergies must be an array");
+                }
+            },
+        },
     },
     diets: {
         type: DataTypes.JSON,
         allowNull: true,
+        validate: {
+            isArray(value) {
+                if (value !== null && !Array.isArray(value)) {
+                    throw new Error("Diets must be an array");
+                }
+            },
+        },
     },
     starCount: {
         type: DataTypes.INTEGER,
