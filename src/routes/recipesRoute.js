20ba3619c2654e67ab40b7cf85ad115a4abf2f4e
herb <wujowujo3@gmail.com>
2024-07-31 17:20:09 +0200

    star recipe endpoint v0.5.3

--- src/routes/recipesRoute.js
+++ src/routes/recipesRoute.js
@@ -37,4 +37,35 @@ router.get("/recipes/:id", async (req, res) => {
     }
 });
 
+/**
+ * Route to star a recipe
+ * @name POST /recipes/:id/star
+ * @function
+ * @memberof module:recipp_app
+ * @inner
+ * @async
+ * @param {object} req Express request object
+ * @param {object} res Express response object
+ */
+router.post("/recipes/:id/star", async (req, res) => {
+    try {
+        const recipe = await Recipe.findByPk(req.params.id);
+        if (recipe) {
+            recipe.starCount = (recipe.starCount || 0) + 1;
+            await recipe.save();
+            res.json({
+                message: "Recipe starred successfully",
+                starCount: recipe.starCount,
+            });
+        } else {
+            res.status(404).json({ message: "Recipe not found" });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "An error occurred while starring the recipe",
+            error: error.message,
+        });
+    }
+});
+
 module.exports = router;
